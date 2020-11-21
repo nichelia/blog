4fbaba57fb84c3db802ebd5f12bcef48a8c9ee2c
Nicholas Elia <nich.elia@gmail.com>
2020-11-21 20:08:06 +0000

    Improvements on post detail view

--- blog/src/app/posts/post-detail/post-detail.component.ts
+++ blog/src/app/posts/post-detail/post-detail.component.ts
@@ -1,17 +1,16 @@
-import { Component, ElementRef, OnInit } from '@angular/core'
-import { ActivatedRoute } from '@angular/router'
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
-import { PostService } from '../post.service'
-import { Post } from '../post'
+import { PostService } from '../post.service';
+import { Post } from '../post';
 
 @Component({
   selector: 'app-post-detail',
   templateUrl: './post-detail.component.html',
   styleUrls: ['./post-detail.component.css'],
 })
-export class PostDetailComponent implements OnInit
-{
-  post: Post
+export class PostDetailComponent implements OnInit {
+  post: Post;
   headings: Element[];
 
   constructor(
@@ -20,60 +19,50 @@ export class PostDetailComponent implements OnInit
     private postService: PostService
   ) {}
 
-  ngOnInit()
-  {
+  ngOnInit() {
     this.getPost();
   }
 
-  getPost(): void
-  {
+  getPost(): void {
     const id = this.route.snapshot.paramMap.get('id');
-    this.postService.getPostData(id).subscribe(post => (this.post = post));
+    this.postService.getPostData(id).subscribe((post) => (this.post = post));
   }
 
-  onLoad(): void
-  {
+  onLoad(): void {
     this.stripContent();
-    this.setWidth();
     this.setHeadings();
   }
 
-  private stripContent(): void
-  {
+  private stripContent(): void {
     this.elementRef.nativeElement
       .querySelector('markdown')!
-      .querySelectorAll('markdown > p:nth-child(-n + 2), #ngx-markdown, #table-of-contents + ul, #table-of-contents')
-      .forEach(x => x.remove());
+      .querySelectorAll(
+        'markdown > p:nth-child(-n + 2), #ngx-markdown, #table-of-contents + ul, #table-of-contents'
+      )
+      .forEach((x) => x.remove());
   }
 
-  private setWidth(): void
-  {
-    this.elementRef.nativeElement
-      .querySelector('markdown')!
-      .querySelectorAll('p, pre, li')
-      .forEach(x => {
-        console.log(x);
-        // x.style["width"] = "calc(100%/var(--grid_columns)*6 - var(--grid_gutter)*0.5)";
-        // x.style["padding-right"] = "calc(var(--grid_gutter)*0.5)";
-      });
-  }
-
-  private setHeadings(): void
-  {
+  private setHeadings(): void {
     const headings: Element[] = [];
+    // Populate headings as sidenav links
     this.elementRef.nativeElement
       .querySelector('markdown')!
       .querySelectorAll('h2')
-      .forEach(x => {
+      .forEach((x) => {
         headings.push(x);
-        x.style["width"] = "calc(100%/var(--grid_columns)*6 - var(--grid_gutter)*0.5)";
-        x.style["padding-right"] = "calc(var(--grid_gutter)*0.5)";
-        x.style["margin-bottom"] = "20px";
-        x.style["margin-top"] = "4.16667vw";
-        x.style["font-size"] = "var(--h6)";
-        x.style["line-height"] = "var(--h6-line_height)";
-        x.style["letter-spacing"] = "var(--h6-letter_spacing)";
+        x.style['padding-right'] = 'calc(var(--grid_gutter)*0.5)';
+        x.style['margin-bottom'] = '20px';
+        x.style['margin-top'] = '4.16667vw';
+        x.style['font-size'] = 'var(--h6)';
+        x.style['line-height'] = 'var(--h6-line_height)';
+        x.style['letter-spacing'] = 'var(--h6-letter_spacing)';
       });
+
+    // Remove title of markdown
+    const title = this.elementRef.nativeElement.querySelector('markdown h1');
+    if (title) {
+      title.remove();
+    }
 
     this.headings = headings;
   }
