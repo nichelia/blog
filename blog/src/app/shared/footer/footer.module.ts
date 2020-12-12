5c07200d0e67523f6bd0a8ba26ccc27532f2cdaf
Nicholas Elia <nich.elia@gmail.com>
2020-12-12 12:34:39 +0000

    Added info module as part of the footer - moved it to shared

--- blog/src/app/shared/footer/footer.module.ts
+++ blog/src/app/shared/footer/footer.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { InfoModule } from '@shared/info/info.module';
 import { FooterComponent } from './footer.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    InfoModule,
   ],
   declarations: [FooterComponent],
   exports: [FooterComponent],
